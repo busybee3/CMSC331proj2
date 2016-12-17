091d2f52c9081b8ec587274c1a516350abf2bf10
Dustin Cuocci <dcuocci1@umbc.edu>
2016-12-17 14:00:57 -0500

    Added password reset. Modified register.php and editInfo.php to include correct degree types. Modified questions/plans page after main registration page.

--- student/studentconfirmed.php
+++ student/studentconfirmed.php
@@ -29,7 +29,7 @@ $rs = $COMMON->executeQuery($sql,$fileName);
 $row = mysql_fetch_row($rs);
 
 
-if (!$_POST) { ?>
+if (!$_POST) { ?> 
 
   <div class="main-form">
     <div id="greeting-text"> 
@@ -37,27 +37,18 @@ if (!$_POST) { ?>
     </div>
   </div>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" name="qpform">
 
-<div class="main">
+<div class="main">  
 
-  <center>
-
-    Now that your account has been created, please take the time to answer<br>
-    the following questions, which will be of great benefit to your advisor<br>
-    during this process.<br><br>
+    Please complete the form(s).<br><br>
   
-    What are your current post-UMBC plans? For example: Medical School, Teach<br>
-    middle school science, Research career, Master's/PhD, etc. (max length: 128 characters)<br>
-    
-    <input type="text" align="center" name="futurePlans" maxlength="128" style="width:450px"><br><br>
-   
-    
-    Do you have any questions or concerns that you would like to discuss during<br>
-    your advising session? For example: Withdrawing from a course, adding a<br>
-    second major, etc. (max length: 128 characters)<br>
-    
-    <input type="text" align="center" name="advisingQuestions" maxlength="128" style="width:450px"><br>
+    What are your current post-UMBC plans?<br>
+    <textarea rows="4" cols="50" name="futurePlans" maxlength="128" placeholder="For example: Medical School, Teach middle school science, Research career, Master's/PhD, etc."></textarea><br><br>
+        
+    Do you have any questions or concerns that you would like to discuss during
+    your advising session?<br>
+    <textarea rows="4" cols="50" name="advisingQuestions" maxlength="128" placeholder="For example: Withdrawing from course, adding a second major, etc..."></textarea><br><br>
 
 
     <input type="submit" value="SUBMIT" name="Register" class="submit" style="color: white; border: none; font-family: Arial, sans-serif; font-size: 20px; width: 120px; line-height: 25px; margin: 0 auto; padding: 10px 0;">
@@ -67,7 +58,7 @@ if (!$_POST) { ?>
       <input type="submit" value="RETURN" name="Return" class="submit" style="color: white; border: none; font-family: Arial, sans-serif; font-size: 20px; width: 120px; line-height: 25px; margin: 0 auto; padding: 10px 0;">
     </form>
 
-  </center>
+  
 </div>
 
   <?php
@@ -76,9 +67,18 @@ if (!$_POST) { ?>
 
 else if ($_POST) {
 
-  $futurePlans = $_POST['futurePlans'];
 
-  $advisingQuestions = $_POST['advisingQuestions'];
+  if (isset($_POST["futurePlans"])){ 
+  
+    $futurePlans = $_POST["futurePlans"];
+
+  }
+
+  if (isset($_POST["advisingQuestions"])){
+
+    $advisingQuestions = $_POST["advisingQuestions"];
+
+  }
 
   if (strlen($futurePlans) <= 1) {
 
@@ -119,6 +119,13 @@ else if ($_POST) {
 
 </div>
 
+<script>
+
+function clearContents(element) {
+  element.value = '';
+}
+
+</script>
 
 </body>
 </html>
